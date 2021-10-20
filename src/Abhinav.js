4e95a94600e8caaea991fea47859417087913729
Abhinav Gorantla <abhinavgorantla@pop-os.localdomain>
2021-10-20 17:15:04 +0530

    ADD: Code for abhinav card

--- src/Abhinav.js
+++ src/Abhinav.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './App.css';
 
 function Abhinav(props) {
     return (
-        <div>
-            Abhinav
-            {/* Enter HTML Here */}
+        <div className='cardView'>
+        <div className="card">
+          <div className="cardImage">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png" />
+          </div>
+          <div className="cardText">
+            <h4>Charizard</h4>
+            <p><strong>Height</strong> 5'07"</p>
+            <p><strong>Category</strong> Flame</p>
+            <p><strong>Weight</strong> 199.5 lbs</p>
+            <p><strong>Abilities</strong> Blaze</p>
+          </div>
         </div>
+      </div>
     );
 }
 
